08537000b5d0c595ac657d8511137ec444715940
Matt Cottrell <mattcottrell93@hotmail.co.uk>
2017-04-04 10:46:38 +0000

    Added user profiles form

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -2,6 +2,7 @@ class ProfilesController < ApplicationController
     
     # GET to /user/:user_id/profile/new
     def new
-       # Render blank profile details form 
+       # Render blank profile details form
+       @profile = Profile.new
     end
 end
